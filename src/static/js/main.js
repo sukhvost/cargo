6f1d31c3df812b15455ed497196fa6628fab0a52
Melnikov Viktor <sukhvost@yandex.ru>
2020-03-12 23:29:33 +0300

    add function toogler on section_offers

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,16 @@
-document.querySelector('.arrow').onclick = () => {
-    document.querySelector('.section_offers').style.display = 'block';
+let btn = document.querySelector('.arrow');
+let blockOffers = document.querySelector('.section_offers');
+// let visibleBlock = document.querySelector('.js_section_offers_block');
+
+
+btn.onclick = toggler;
+
+function toggler() {
+    if (blockOffers.classList.contains('js_section_offers_none')) {
+        blockOffers.classList.remove('js_section_offers_none');
+        blockOffers.classList.add('js_section_offers_block');
+    } else if (blockOffers.classList.contains('js_section_offers_block')) {
+        blockOffers.classList.remove('js_section_offers_block');
+        blockOffers.classList.add('js_section_offers_none');
+    }
 }
